355218f4b2481453216e29e196d0df30776a986a
ZETREY <84082026+ZHKING13@users.noreply.github.com>
2024-08-09 17:23:14 +0000

    change config file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            treeshake: {
+                // Utilisation d'une option plus permissive pour le tree-shaking
+                moduleSideEffects: 'no-external',
+            },
+        },
+    },
 })
