6d7fadc6a0d34ceee9f2d89bf8b349458c91f8d5
Mel Cedric Datcha <152630291+Akuma225@users.noreply.github.com>
2024-08-16 13:11:29 +0000

    Refactor GuestRoute component to fix first login redirection bug

--- src/feature/Guards/GuestRoute.jsx
+++ src/feature/Guards/GuestRoute.jsx
@@ -2,19 +2,19 @@
 import React from "react";
 import { Route, Navigate } from "react-router-dom";
 
-const GuestRoute = ({element}) => {
+const GuestRoute = ({ element }) => {
     // Check if the user is authenticated
     const isAuthenticated = localStorage.getItem("isLog");
     const logUser = JSON.parse(localStorage.getItem("user"));
     const isFirstLogin = localStorage.getItem("firstLogin")
     if (isAuthenticated && isFirstLogin === "true") {
-        
-        return  <Navigate to="/firstlogin"/>
-}
-     if (isAuthenticated && logUser.profile !== "Client") {
+
+        return <Navigate to="/firstlogin" />
+    }
+    if (isAuthenticated && logUser.profile !== "Client") {
         return element;
     } else {
-        return <Navigate to="/login"/>
+        return <Navigate to="/login" />
     }
 };
 
